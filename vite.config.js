cfd98f80d735bc58e7f3b272453c0765c86f9009
Mingx1994x <mggxmingx@gmail.com>
2025-08-04 22:40:06 +0800

    test: 調整部署環境路徑

--- vite.config.js
+++ vite.config.js
@@ -16,5 +16,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: 'https://mingx1994x.github.io/HS_Vue_practice/'
+  base: /HS_Vue_practice/
 })
